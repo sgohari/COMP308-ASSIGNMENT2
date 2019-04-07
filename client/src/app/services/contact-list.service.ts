5a84b288cc8388bd36634209d274e3a5cd72473a
sgohary <syednacer@gmail.com>
2019-04-06 21:14:35 -0400

    dbConnection string changes

--- client/src/app/services/contact-list.service.ts
+++ client/src/app/services/contact-list.service.ts
@@ -20,7 +20,7 @@ export class ContactListService {
   private user: User;
   private authToken: any = null;
 
-  private endpoint = 'http://comp308-w19-assignment2.herokuapp.com/api/contact/contact-list/';
+  private endpoint = 'http://comp308-w19-assignment2.herokuapp.com/api/contact-list/';
 
   private httpOptions = {
     headers: new HttpHeaders({
